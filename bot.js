de6d78e82c40676c418506ec117189e3c9c64130
yehiagaming11 <48064035+yehiagaming11@users.noreply.github.com>
2019-02-27 19:13:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -155,7 +155,7 @@ client.on("message", async message => {
             معلومات عن السيرفر : ${prefix}server
             برودكاست للأونلاين فقط : ${prefix}bco
             يعرض لك عدد المتبندين من سيرفرك : ${prefix}banned
-            رابط سيرفر الدعم الفني : https://discord.gg/YEXcDXt 
+            رابط سيرفر الدعم الفني : https://discord.gg/8BnpFwk 
             **`);
             message.channel.sendEmbed(help); // رابط السيرفر يعود الى سيرفر CODES .
     }
